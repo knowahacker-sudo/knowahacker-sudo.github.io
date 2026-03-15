46dd23624aea24e3480a161bbde7af0ba35889a3
knowahacker-sudo <knowahacker@gmail.com>
2026-03-15 18:03:46 -0400

    Create 9_settings.py

---
+++ discord.clone/config/9_settings.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_WEBHOOK_URL = os.environ.get("https://discord.com/api/webhooks/1482859214895579258/x10n-msqCo7M31DlpZn04j9z9-PTT3zQsdR_ML2IFqTDH6_M4DZUP1xqczTpNEXctcpP")
